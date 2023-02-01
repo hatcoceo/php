5292d8f319d671abf5c5b05f74a3a5d7c28df378
hatcoceo <39942640+hatcoceo@users.noreply.github.com>
2023-02-01 20:24:35 +0700

    Update lib.php

--- lib.php
+++ lib.php
@@ -5,6 +5,10 @@ function create(){
 function read(){
 }
 function update(){
+
+  $sql = "UPDATE table_name SET col_a = '$col_a', col_b = '$col_a', col_c = '$col_c' WHERE id = $url_id ";
+	$qr = mysqli_query($connect, $sql);
+  header('location:page.php?id='.$id_tieu_de);
 }
 function delete(){
 }
