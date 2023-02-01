5dfe06fa5c14948cf3a399187fe1bb51904f43b7
hatcoceo <39942640+hatcoceo@users.noreply.github.com>
2023-02-01 20:01:05 +0700

    Update and rename functions.php to lib.php

---
+++ lib.php
@@ -0,0 +1,10 @@
+global connect;
+$connect = mysqli_connect('localhost', 'id18044425_vinthink1', '241432679vV!', 'id18044425_vinthink');
+function create(){
+}
+function read(){
+}
+function update(){
+}
+function delete(){
+}
