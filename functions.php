835a73604929c80c27f1e373db67af9112fc7429
hatcoceo <39942640+hatcoceo@users.noreply.github.com>
2023-02-11 18:57:42 +0700

    Update and rename lib.php to functions.php

--- functions.php
+++ functions.php
@@ -4,17 +4,28 @@ require 'header.php';
 require 'condb.php';
 
 global connect;
+
 $connect = mysqli_connect('localhost', 'id18044425_vinthink1', '241432679vV!', 'id18044425_vinthink');
 function create(){
 }
+
 function read(){
 }
+
 function update($table, $id){
 
   $sql = "UPDATE $table SET col_a = '$col_a', col_b = '$col_a', col_c = '$col_c' WHERE id = $id ";
   $qr = mysqli_query($connect, $sql);
   header('location:page.php?id='.$id);
 }
+
 function delete(){
+}
+
+function get_id_post(){
+}
+
+function get_id_categories(){
+
 }
  ?>
