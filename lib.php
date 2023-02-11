481219e642bd8ecb707522fd4d6e2a35460ba75e
hatcoceo <39942640+hatcoceo@users.noreply.github.com>
2023-02-11 18:54:04 +0700

    Update lib.php

--- lib.php
+++ lib.php
@@ -3,20 +3,18 @@ require 'header.php';
 
 require 'condb.php';
 
-
- ?>
-
 global connect;
 $connect = mysqli_connect('localhost', 'id18044425_vinthink1', '241432679vV!', 'id18044425_vinthink');
 function create(){
 }
 function read(){
 }
-function update(){
+function update($table, $id){
 
-  $sql = "UPDATE table_name SET col_a = '$col_a', col_b = '$col_a', col_c = '$col_c' WHERE id = $url_id ";
-	$qr = mysqli_query($connect, $sql);
-  header('location:page.php?id='.$id_tieu_de);
+  $sql = "UPDATE $table SET col_a = '$col_a', col_b = '$col_a', col_c = '$col_c' WHERE id = $id ";
+  $qr = mysqli_query($connect, $sql);
+  header('location:page.php?id='.$id);
 }
 function delete(){
 }
+ ?>
