8370005f42b9e34a3b841040e2809f5ae7473079
hatcoceo <39942640+hatcoceo@users.noreply.github.com>
2023-02-11 19:26:31 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,9 +7,8 @@ global $connect;
 
 $connect = mysqli_connect('localhost', 'id18044425_vinthink1', '241432679vV!', 'id18044425_vinthink');
 
-function qr(){
-  global $connect;
-  mysqli_query($connect, $sql);
+function qr($sql){
+
 }
 
 function create(){
