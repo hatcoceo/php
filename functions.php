03b923981b65c60bdd3bda2d2e213bc255db3d32
hatcoceo <39942640+hatcoceo@users.noreply.github.com>
2023-05-10 21:02:07 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,70 +1,7 @@
 
 
-$connect = mysqli_connect('localhost', 'id18044425_vinthink1', '241432679vV!', 'id18044425_vinthink');
-
-function get_url_value($id){
-    if(isset ($_GET[$id])){
-        $id =$_GET[$id];
-        return $id;
-    }
-}
-
-
-function get_post_value($name){
-    if(isset ($_POST[$name])){
-        
-            
-        $name = $_POST[$name];
-        return $name;
-        
-    }
-}
-
-$userTable =[
-       'username' => $username , 
-       'password' => $password 
-    ] ;
-   
-
-
-function insert ($table, $userTable, $connect ){
-   
-   
-
-$intoUser = implode(",",array_keys ($userTable));
- 
-$valueUser = implode ("', '", array_values ($userTable));
- $sql = "INSERT INTO $table($intoUser) VALUES ('$valueUser') ";
-    
-    $qr = mysqli_query ($connect, $sql);
-   if($qr){
-       echo "lưu thành công";
-   }else {
-       echo "chưa lưu được ".mysqli_error($connect );
-   }
-   mysqli_close($connect);
-}
-
-  // Function to get the columns of a table in a database
-  
-function get_columns($table, $connect ) {
-    // Code to connect to the database
-   
-
-    // Get the columns of the table
-    $sql = "SHOW COLUMNS FROM $table";
-    $qr= mysqli_query($connect , $sql);
-
-    $columns = [];
-    while ($f_a = mysqli_fetch_array ($qr)) {
-        $columns[] = $f_a['Field'];
-    }
-   mysqli_close($connect);
-    
-    return $columns;
-}
 
 
     
 
-?> 
+
